5e2dfadd531bd3accd71056e7817bb2d734b1f4f
simesquivel <159603772+simesquivel@users.noreply.github.com>
2026-09-07 11:48:19 +0530

    Add: maximum-product-subarray - submission-0

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int ans = INT_MIN;
+        int n = nums.size();
+        int prefix = 1;
+        int suffix = 1;
+
+        for(int i=0; i<nums.size(); i++){
+            if(prefix == 0) prefix = 1;
+            if(suffix == 0) suffix = 1;
+
+            prefix *= nums[i];
+            suffix *= nums[n-i-1];
+
+            ans = max(ans, max(prefix, suffix));
+        }
+
+        return ans;
+    }
+};
